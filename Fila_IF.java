3d5f64d770451bc1129b2ae13f4ae4be9534670a
Letícia Montenegro <montenegro.letiicia@gmail.com>
2024-10-14 20:39:50 -0300

    feat: Fila_IF

--- Fila_IF.java
+++ Fila_IF.java
@@ -1,3 +1,10 @@
 public interface Fila_IF {
-    
+
+    public Filme_IF dequeue() throws Exception;
+
+    public void enqueue(Filme_IF elemento);
+
+    public boolean isEmpty();
+
+    public Filme_IF head() throws Exception;
 }
